9d99c18fc6b660c9491d3fa01948ff992313f10d
ephielam <ephielam@gmail.com>
2021-12-02 11:59:40 +0000

    Test webhook

--- builder.php
+++ builder.php
@@ -14,7 +14,7 @@
     <h1>KO Single-platform Bootstrap Builder</h1>
     <div>
       <div>
-        <p class="header">Select the component you wish to build:</p>
+        <p class="header"><b>Select the component you wish to build:</b></p>
         <ul class="nav nav-tabs">
           <?php
             $components = array('Carousel', 'Collapse', 'Timeline', 'Table', 'Transcript', 'Card', 'List',
