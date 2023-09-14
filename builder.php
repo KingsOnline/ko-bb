96d404ad408bd2324a909a2f519cf56a759526ca
ephielam <ephielam@gmail.com>
2023-09-14 15:26:32 +0100

    Remove old theme CSS and update Bootstrap CSS

--- builder.php
+++ builder.php
@@ -1,10 +1,9 @@
 <html>
   <head>
     <title>KO Bootstrap Builder</title>
-    <link href="https://keats.kcl.ac.uk/theme/styles.php/lambda/1603362316_1602571112/all" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
     <link href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" rel="stylesheet">
-    <link href="https://git.iddkingsonline.com/designsystem/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://git.iddkingsonline.com/designsystem-dev/css/main.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
   </head>
